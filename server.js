5e8456815047102a6932ee425d00add58964e3ca
mohammedafeef <mohammedafeef.maf@gmail.com>
2021-01-25 20:13:36 +0530

    javascript is properly synced with the html

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //selecting the elements form the dom
 let quizQuestion = document.querySelector(".quizQuestion span");
-let quizAnswers = document.querySelectorAll(".quizAnswers label");
+let quizAnswers = document.querySelectorAll(".quizAnswers input");
+let quizAnswersLabel = document.querySelectorAll(".quizAnswers label");
 let quizSubmitionButton = document.querySelector(".quizAnswers button");
 let quizgameOverSection = document.querySelector(".gameOver");
 let quizGameSection = document.querySelector(".game");
 let quizPoint = document.querySelector(".correct")
-let quizLossPoint = documet.querySelector(".wrong");
+let quizLossPoint = document.querySelector(".wrong");
+let out = console.log;
 //question for the quiz 
 const questions = [
     {
@@ -40,8 +42,8 @@ let currentQusIndex = points = 0;
 const nextQustion = () =>{
     let currentQus = questions[currentQusIndex];
     quizQuestion.innerHTML = currentQus.qus ;
-    quizAnswers.forEach((answer) => {
-        answer.innerHtML = currentQus[answer.value];
+    quizAnswersLabel.forEach((answer) => {
+        answer.innerHTML = currentQus[`${answer.id}`];
     })
     currentQusIndex == questions.length - 1 ?quizSubmitionButton.innerHTML = "submit" : null ;
     
@@ -50,8 +52,8 @@ const nextQustion = () =>{
 }
 //for ending the game
 const gameOver = () =>{
-    quizPoint.innerHTML = point;
-    quizLossPoint.innerHTML = questions.length - 1 - point ;
+    quizPoint.innerHTML = points;
+    quizLossPoint.innerHTML = questions.length - points ;
     quizGameSection.style.display = "none";
     quizgameOverSection.style.transform = "translateX(0%)";
     currentQusIndex = points = 0;
@@ -62,9 +64,9 @@ const answerValidation = (userAnswer) =>{
     let userSelectedAns = userAnswer.value;
     let correctAns = questions[currentQusIndex].ans;
     if ( correctAns == userSelectedAns){
-        point++;
+        points++;
     }
-    if (!(questions.length() > currentQusIndex)){
+    if (questions.length - 1 > currentQusIndex){
         currentQusIndex++;
         nextQustion();
     }else{
@@ -73,12 +75,14 @@ const answerValidation = (userAnswer) =>{
 }
 //for listing to the click event and find the selected option
 const answerSubmition = () =>{
-    if (questions.length() > currentQusIndex){
+    out("called");
+    if (questions.length > currentQusIndex){
         quizAnswers.forEach((answer)=>{
             if (answer.checked == true){
+                out("answer called");
                 answerValidation(answer);
-                break;
             }
         })
     }
 }
+quizSubmitionButton.addEventListener("click",()=>{answerSubmition()});
